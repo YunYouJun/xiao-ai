cb3ce102aa8dd1a97d65aededb8effd94de7e498
YunYouJun <910426929@qq.com>
2018-01-01 01:28:21 +0800

    add init custom

---
+++ custom.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+def onQQMessage(bot, contact, member, content):
+    if content == '-hello':
+        bot.SendTo(contact, '你好，我是云游酱的女朋友~')
+    elif content == '-stop':
+        bot.SendTo(contact, '小爱退下啦~')
+        bot.Stop()
+
+    if '小爱' in content and not bot.isMe(contact, member) :
+      if '你好' in content:
+        bot.SendTo(contact, '你好，我是云游酱的女朋友~')
+      elif '智障' in content:
+        bot.SendTo(contact, '你才智障！')
+      elif '吃鸡吗' in content:
+        bot.SendTo(contact, '不吃，那有啥可吃的，都是皮。')
+      elif '新年快乐' in content or '元旦快乐' in content:
+        bot.SendTo(contact, '你也是哦，新年快乐哟~')
+      elif '云游呢' in content:
+        bot.SendTo(contact, '不告诉你！')
+      elif '我可爱吗' in content:
+        bot.SendTo(contact, '当然可爱呀！')
+      elif '你喜欢云游吗' in content:
+        bot.SendTo(contact, '当然呀！')
+      else:
+        bot.SendTo(contact, '干啥呀？')
+
+    if '睿神不在' in content or '云游不在' in content or '云游酱不在' in content or '云游君不在' in content:
+        bot.SendTo(contact, '略略略')
+    elif '云游的女朋友' in content:
+        bot.SendTo(contact, '叫我干啥？')
+    elif '不太好' in content:
+      bot.SendTo(contact, '怎么不好？')
