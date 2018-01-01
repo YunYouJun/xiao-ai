3c0b1da0e8222bce0b6ffaf5bed3ed7806e83447
YunYouJun <910426929@qq.com>
2018-01-01 12:24:06 +0800

    deploy auto

--- webhook.py
+++ webhook.py
@@ -4,7 +4,7 @@ def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
     DeployPath = '/root/.qqbot-tmp/plugins'
     os.system('cd ' + DeployPath)
-    os.system('git fetch --all')
-    os.system('git reset --hard origin/master')
+    os.system('git reset --hard')
+    os.system('git pull')
     os.system('qq plug custom')
     print('XiaoAi pull finish')
