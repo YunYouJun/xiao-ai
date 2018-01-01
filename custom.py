7ceeb0111409a3ed481478bfe642f10119d18c1f
YunYouJun <910426929@qq.com>
2018-01-01 18:11:04 +0800

    like who

--- custom.py
+++ custom.py
@@ -34,9 +34,10 @@ def onQQMessage(bot, contact, member, content):
       elif '你是谁' in content:
         bot.SendTo(contact, '你好，我是小爱，云游酱的女朋友~')
       elif '喜欢谁' in content:
-        bot.SendTo(contact, '当然是云游酱啦~')
-      elif '不喜欢谁' in content:
-        bot.SendTo(contact, '不告诉你')
+        if '不' in content:
+          bot.SendTo(contact, '小爱还没考虑那么多……')
+        else:
+          bot.SendTo(contact, '当然是云游酱啦~')
       elif '吃' in content and '吗' in content or '吧' in content:
         if '鸡' in content:
           bot.SendTo(contact, '不吃，那有啥可吃的，都是皮。')
