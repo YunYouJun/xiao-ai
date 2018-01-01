c091517e571b3d38b34b5a4a505ce9074bdf0eaa
YunYouJun <910426929@qq.com>
2018-01-01 12:21:25 +0800

    deploy auto

--- start_webhook.py
+++ start_webhook.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 from wsgiref.simple_server import make_server
-# 导入我们自己编写的application函数:
+# 导入我们自己编写的application函数
 from webhook import application
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
 httpd = make_server('', 8000, application)
--- webhook.py
+++ webhook.py
@@ -2,5 +2,9 @@
 import os
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    os.system('./deploy.sh')
+    DeployPath = '/root/.qqbot-tmp/plugins'
+    os.system('cd ' + DeployPath)
+    os.system('git fetch --all')
+    os.system('git reset --hard origin/master')
+    os.system('qq plug custom')
     print('XiaoAi pull finish')
