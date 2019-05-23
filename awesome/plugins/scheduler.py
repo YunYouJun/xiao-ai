48ddc67807e4bf32d5da13a1023a975a0d7d26e1
YunYouJun <me@yunyoujun.cn>
2019-05-23 22:41:14 +0800

    feat: add interval

--- awesome/plugins/scheduler.py
+++ awesome/plugins/scheduler.py
@@ -4,13 +4,29 @@ import nonebot
 import pytz
 from aiocqhttp.exceptions import Error as CQHttpError
 
-my_group=102588810
+group = {
+    'feiwu': 83765557,
+    'lost_time': 102588810
+}
 
 @nonebot.scheduler.scheduled_job('cron', hour='*')
 async def _():
     bot = nonebot.get_bot()
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
     try:
-        await bot.send_group_msg(group_id=my_group, message=f'现在{now.hour}点整啦！熬夜一时爽，一直熬夜一直爽！')
+        await bot.send_group_msg(group_id=group['lost_time'], message=f'现在{now.hour}点整啦！熬夜一时爽，一直熬夜一直爽！')
+    except CQHttpError:
+        pass
+
+
+drink_time = 0
+@nonebot.scheduler.scheduled_job('interval', minutes=10)
+async def _():
+    global drink_time
+    drink_time += 1
+    bot = nonebot.get_bot()
+    # now = datetime.now(pytz.timezone('Asia/Shanghai'))
+    try:
+        await bot.send_group_msg(group_id=group['feiwu'], message=f'大家好，我是本群的“提醒喝水小助手”，这是今天的第{drink_time}轮。希望此刻看到消息的人可以和我一起来喝一杯水。10分钟后的我继续提醒大家喝水。和我一起成为一天48杯水的人吧！多喝热水。谢谢合作！')
     except CQHttpError:
         pass
--- config.py
+++ config.py
@@ -5,4 +5,8 @@ HOST = '0.0.0.0'
 PORT = 9969
 
 SUPERUSERS = {910426929}
-COMMAND_START = ['', re.compile(r'[♂/!]+')]
+COMMAND_START = ['', re.compile(r'[-/!]+')]
+
+NICKNAME = {'小爱', '爱醬'}
+# 无所谓的 KEY 2333
+TULING_API_KEY = '41ae25f7550247d5b270cdc93fd3b25d'
