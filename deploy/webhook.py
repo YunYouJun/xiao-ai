daac2e7389eb4620eedd55df136996018dc01f9c
YunYouJun <me@yunyoujun.cn>
2019-05-23 22:45:00 +0800

    fix: deploy path

--- deploy/webhook.py
+++ deploy/webhook.py
@@ -3,7 +3,7 @@
 import os
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    DeployPath = '/root/github/xiao-ai'
+    DeployPath = '/home/yunyou/github/xiao-ai/'
     os.system('cd ' + DeployPath)
     os.system('git pull --force')
     print('xiao-ai pull finish')
