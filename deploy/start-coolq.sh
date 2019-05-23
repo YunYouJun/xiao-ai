c783eb1eeb522b82b19267a7a28d4104f944221f
YunYouJun <me@yunyoujun.cn>
2019-05-23 21:34:12 +0800

    trigger

--- deploy/start-coolq.sh
+++ deploy/start-coolq.sh
@@ -7,6 +7,6 @@ docker run -ti --rm --name cqhttp-xiaoai \
             -e CQHTTP_SERVE_DATA_FILES=yes \
             -e VNC_PASSWD=MAX8char \
             -e CQHTTP_USE_WS_REVERSE=true \
-            -e CQHTTP_WS_REVERSE_API_URL=ws://172.17.0.1:8080/ws/api/ \
-            -e CQHTTP_WS_REVERSE_EVENT_URL=ws://172.17.0.1:8080/ws/event/ \
+            -e CQHTTP_WS_REVERSE_API_URL=ws://172.17.0.1:9969/ws/api/ \
+            -e CQHTTP_WS_REVERSE_EVENT_URL=ws://172.17.0.1:9969/ws/event/ \
             richardchien/cqhttp:latest
